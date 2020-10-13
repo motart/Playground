9206fbdc37b9d71fcd9f218619539850f698d8a2
Rachid Abadli <rachidabadli@Rachids-MacBook-Pro.local>
2020-10-13 15:54:55 -0700

    Added BST verifier exercise

--- src/main/java/com/playground/IsBinarySearchTree.java
+++ src/main/java/com/playground/IsBinarySearchTree.java
@@ -3,22 +3,46 @@ package com.playground;
 import java.util.TreeSet;
 
 public class IsBinarySearchTree {
+    /* Hidden stub code will pass a root argument to the function below. Complete the function to solve the challenge. Hint: you may want to write one or more helper functions.
 
+    The Node class is defined as follows:
+        class Node {
+            int data;
+            Node left;
+            Node right;
+         }
+    */
+    TreeSet<Integer> data(Node root) {
+        TreeSet<Integer> result = new TreeSet<>();
+        if (root == null) {
+            return result;
+        }
+        if (root.right == null && root.left == null) {
+            result.add(root.data);
+        }
+        result.addAll(data(root.right));
+        result.addAll(data(root.left));
+        return result;
+    }
 
+    int leftData(Node root) {
+        return data(root).last();
+    }
 
+    int rightData(Node root) {
+        return data(root).first();
+    }
 
     boolean checkBST(Node root) {
-        TreeSet<Integer> tSet = new TreeSet<>();
-        if (root == null || (root.right == null && root.left == null)) {
-            return false;
-        } else
-
-        if (root.data > root.left.data && root.data < root.right.data) {
-            return true;
+        boolean isBST;
+        if (root.left != null && root.right != null) {
+            isBST = root.data > leftData(root.left) && root.data < rightData(root.right);
+            if (!isBST) {
+                return false;
+            }
+            return checkBST(root.left) && checkBST(root.right);
         }
-
-        return checkBST(root.left) && checkBST(root.right);
-
+        return true;
     }
 
     static class Node {
