908345ace031efbcb436f8d10469511b5e4822e5
Rachid Abadli <rachidabadli@Rachids-MacBook-Pro.local>
2020-10-13 16:48:44 -0700

    Added gitignore

--- src/main/java/com/playground/InsertIntoBST.java
+++ src/main/java/com/playground/InsertIntoBST.java
@@ -1,4 +1,60 @@
 package com.playground;
 
+import java.util.Scanner;
+
 public class InsertIntoBST {
+    public static void preOrder( Node root ) {
+
+        if( root == null)
+            return;
+
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+
+    }
+
+    public static Node insert(Node root,int data) {
+        if (root == null) {
+            return new Node(data);
+        }
+        if (root.data > data) {
+            if (root.left == null) {
+                root.left = new Node(data);
+            } else {
+                insert(root.left,data);
+            }
+        } else {
+            if (root.right == null) {
+                root.right = new Node(data);
+            } else {
+                insert(root.right,data);
+            }
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        preOrder(root);
+    }
+
+    static class Node {
+        Node left;
+        Node right;
+        int data;
+
+        Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
 }
